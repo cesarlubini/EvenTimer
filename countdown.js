ac90d1cd81e957dcf7db00c837d2922bbe3227fd
Cesar Augusto Lubini <cesar.lubini@gmail.com>
2020-09-16 17:02:19 -0300

    First Commit

---
+++ countdown.js
@@ -0,0 +1,33 @@
+const finalDate = new Date(2020,09,03).getTime();
+
+const $ = document.querySelector.bind(document);
+const dayInput = $('.day');
+const hourInput = $('.hour');
+const minuteInput = $('.minute');
+const secondInput = $('.second');
+
+const countdownInput = $('.countdown');
+const todayInput = $('.today');
+
+let countdown = setInterval(function () {
+    const now = new Date().getTime();
+  
+    const diference = finalDate - now;
+    
+    let days = Math.floor(diference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diference % (1000 * 60)) / 1000);
+  
+    dayInput.innerHTML = days;
+    hourInput.innerHTML = hours;
+    minuteInput.innerHTML = minutes;
+    secondInput.innerHTML = seconds;
+
+    if (diference < 0) {
+      clearInterval(countdown);
+      todayInput.classList.remove('hide');
+      countdownInput.classList.add('hide');
+    }
+
+  },1000);
