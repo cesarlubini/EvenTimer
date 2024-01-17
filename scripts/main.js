94ee82bc957f0562917831398df64270218ee19d
cesarlubini <cesar.lubini@gmail.com>
2024-01-16 21:18:01 -0300

    adicionado frase de blank space, codigo js otimizado, adicionar evento com tecla enter e data do evento formatada

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
 const listaEventos = document.querySelector('.eventosLista');
+const formulario = document.querySelector('.novoEvento-form');
+const eventoNome = formulario.querySelector('#novoEventoNome');
+const eventoData = formulario.querySelector('#novoEventoData');
 const botaoAdicionar = document.querySelector('.novoEventoAdicionar');
+
+// -------------------------- //
 
 let eventos = PegarLocalStorage('eventos') ?? [];
 let eventosId = PegarLocalStorage('eventosId') ?? 0;
+
+montarListaEventos();
+// teste
+converterData('2024-01-27');
+
+botaoAdicionar.addEventListener('click', (e) => adicionarEvento(e));
+formulario.addEventListener('keypress', (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    adicionarEvento(e);
+  }
+});
+
+// -------------------------- //
 
 function PegarLocalStorage(campo) {
   if(localStorage.hasOwnProperty(campo)) {
@@ -10,54 +29,61 @@ function PegarLocalStorage(campo) {
   }
 }
 
-montarListaEventos();
+function montarEvento(evento) {
+
+  let listaItem = document.createElement('li');
+  listaItem.classList.add("eventoLista-item");
+  listaItem.dataset.id = evento.id;
+
+  let listaItemCabecalho = document.createElement('div');
+  listaItemCabecalho.classList.add('eventoLista-item__cabecalho');
+
+  let listaItemNome = document.createElement('div');
+  listaItemNome.classList.add("eventoLista-item__nome");
+  listaItemNome.textContent = evento.nome;
+
+  let listaItemData = document.createElement('div');
+  listaItemData.classList.add("eventoLista-item__data");
+  listaItemData.textContent = converterData(evento.data);
+
+  let listaItemCorpo = document.createElement('div');
+  listaItemCorpo.classList.add('eventoLista-item__corpo');
+
+  let listaItemCronometro = document.createElement('div');
+  listaItemCronometro.classList.add('cronometro');
+  listaItemCronometro.textContent = "00d : 00h : 00m : 00s";
+
+  let listaItemEditar = document.createElement('div');
+  listaItemEditar.classList.add("editarEvento");
+  listaItemEditar.style.background = "url(../assets/icons/editar.svg) no-repeat center";
+
+  let listaItemDelete = document.createElement('div');
+  listaItemDelete.classList.add("deletarEvento");
+  listaItemDelete.style.background = "url(../assets/icons/deletar.svg) no-repeat center";
+  botaoDeletarEvento(listaItemDelete);
+
+  listaItemCabecalho.appendChild(listaItemNome);
+  listaItemCabecalho.appendChild(listaItemData);
+
+  listaItemCorpo.appendChild(listaItemCronometro);
+  listaItemCorpo.appendChild(listaItemEditar);
+  listaItemCorpo.appendChild(listaItemDelete);
+
+  listaItem.appendChild(listaItemCabecalho);
+  listaItem.appendChild(listaItemCorpo);
+
+  return listaItem;
+}
+
 function montarListaEventos() {
-  listaEventos.innerHTML = '';
+  if (eventos.length == 0) {
+    listaEventos.innerHTML = 'Nenhum evento cadastrado.';
+  } else {
+    listaEventos.innerHTML = '';
+  }
 
   eventos.forEach(evento => {
-
-    let listaItem = document.createElement('li');
-    listaItem.classList.add("eventoLista-item");
-    listaItem.dataset.id = evento.id;
-
-    let listaItemCabecalho = document.createElement('div');
-    listaItemCabecalho.classList.add('eventoLista-item__cabecalho');
-
-    let listaItemNome = document.createElement('div');
-    listaItemNome.classList.add("eventoLista-item__nome");
-    listaItemNome.textContent = evento.nome;
-
-    let listaItemData = document.createElement('div');
-    listaItemData.classList.add("eventoLista-item__data");
-    listaItemData.textContent = evento.data;
-
-    let listaItemCorpo = document.createElement('div');
-    listaItemCorpo.classList.add('eventoLista-item__corpo');
-
-    let listaItemCronometro = document.createElement('div');
-    listaItemCronometro.classList.add('cronometro');
-    listaItemCronometro.textContent = "00d : 00h : 00m : 00s";
-
-    let listaItemEditar = document.createElement('div');
-    listaItemEditar.classList.add("editarEvento");
-    listaItemEditar.style.background = "url(../assets/icons/editar.svg) no-repeat center";
-
-    let listaItemDelete = document.createElement('div');
-    listaItemDelete.classList.add("deletarEvento");
-    listaItemDelete.style.background = "url(../assets/icons/deletar.svg) no-repeat center";
-    botaoDeletarEvento(listaItemDelete);
-
-    listaItemCabecalho.appendChild(listaItemNome);
-    listaItemCabecalho.appendChild(listaItemData);
-
-    listaItemCorpo.appendChild(listaItemCronometro);
-    listaItemCorpo.appendChild(listaItemEditar);
-    listaItemCorpo.appendChild(listaItemDelete);
-
-    listaItem.appendChild(listaItemCabecalho);
-    listaItem.appendChild(listaItemCorpo);
-
-    listaEventos.appendChild(listaItem);
+    listaEventos.appendChild(montarEvento(evento));
   })
 }
 
@@ -75,10 +101,8 @@ function botaoDeletarEvento(botao) {
   })
 }
 
-botaoAdicionar.addEventListener('click', () => {
-  const formulario = document.querySelector('.novoEvento-form');
-  const eventoNome = formulario.querySelector('#novoEventoNome');
-  const eventoData = formulario.querySelector('#novoEventoData');
+function adicionarEvento(e) {
+  e.preventDefault();
 
   let evento = {
     id: eventosId,
@@ -97,6 +121,22 @@ botaoAdicionar.addEventListener('click', () => {
   //ajuste para adicionar placeholder no <input type="data"/>
   eventoData.type = 'text';
   
-  montarListaEventos();
-})
+  listaEventos.appendChild(montarEvento(evento));
+}
 
+function converterData(dataEvento) {
+
+  let dataEventoArray = dataEvento.split('-');
+  const data = new Date(dataEventoArray[0], dataEventoArray[1] - 1, dataEventoArray[2]);
+
+  let dataConvertida = [];
+  dataConvertida.push(
+    ("0" + data.getDate()).slice(-2),
+    ("0" + (data.getMonth() + 1)).slice(-2),
+    (data.getFullYear()).toString().slice(-2)
+  );
+  
+  let dataConvertidaRetorno = dataConvertida.join('/');
+
+  return dataConvertidaRetorno;
+}
